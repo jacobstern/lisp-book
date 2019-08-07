f600e57986348b1abe6e8ddf4a08ea2c0c493f0d
Jacob Stern <jacob.stern@outlook.com>
2019-08-06 23:17:28 -0700

    Working 6.1 interpreter minus define

--- chapter6-test.scm
+++ chapter6-test.scm
@@ -4,12 +4,36 @@
 (load "./chapter6.scm")
 
 (define (test-evaluate expr)
-  (define (compile e) (meanint e r.init))
+  (define (compile e) (meaning e r.init))
   (define (run c k) (c sr.init k))
   (call/cc (lambda (k) (run (compile expr) k))))
 
 (test-section "chapter 6.1 interpreter")
 
-(test* "list construction"
-  '(1 2)
-  (test-evaluate '(cons 1 (cons 2 nil))))
+(test* "pair construction"
+  '(1 . 2)
+  (test-evaluate '(cons 1 2)))
+
+(test* "lambda with closed application"
+  '(1 . 2)
+  (test-evaluate '((lambda (x) (cons x 2)) 1)))
+
+(test* "lambda without closed application"
+  '(1 . 2)
+  (test-evaluate '((lambda (f x) (f x)) (lambda (x) (cons x 2)) 1)))
+
+(test* "conditional"
+  '(1 . 2)
+  (test-evaluate '((lambda (f l r) (cons (f l) (f r)))
+                    (lambda (x) (if x 1 2))
+                    t f)))
+
+(test* "set! function argument"
+  '(1 . 2)
+  (test-evaluate '((lambda (x y) (set! x 1) (cons x y)) 2 2)))
+
+(test* "sequence"
+  '(1 . 2)
+  (test-evaluate '((lambda (b x y)
+                     (if b (begin (set! x 1) (cons x y)) (cons x y)))
+                    t 2 2)))
--- chapter6.scm
+++ chapter6.scm
@@ -24,7 +24,8 @@
       (error "Must initialize <activation-frame> with a size"))))
 
 (define (set-activation-frame-argument! x n v)
-  (set! (vector-ref (arguments-of x) n) v))
+  (set! (vector-ref (arguments-of x) n) v)
+  x)
 
 (define activation-frame-argument
   (getter-with-setter (lambda (x n)
@@ -34,8 +35,11 @@
 (define (activation-frame-argument-length x)
   (vector-length (ref x 'arguments)))
 
+(define (allocate-activation-frame n)
+  (make <activation-frame> :next #f :size n))
+
 (define (sr-extend* sr v*)
-  (set! (next-of sr) v*)
+  (set! (next-of v*) sr)
   v*)
 
 (define (deep-fetch sr i j)
@@ -63,7 +67,7 @@
           (local-variable? (cdr r) (+ i 1) n))
         ((eq? n names) `(local ,i . ,j))))))
 
-(define (meaning e )
+(define (meaning e r)
   (if (list? e)
     (case (car e)
       ((quote) (meaning-quotation (cadr e) r))
@@ -75,7 +79,7 @@
     (if (symbol? e) (meaning-reference e r)
       (meaning-quotation e r))))
 
-(define (meaning-quoteation v r)
+(define (meaning-quotation v r)
   (lambda (sr k)
     (k v)))
 
@@ -127,7 +131,7 @@
 
 (define (meaning-some-arguments e e* r size)
   (let ((m (meaning e r))
-         (m* (meaning* e* size))
+         (m* (meaning* e* r size))
          (rank (- size (+ (length e*) 1))))
     (lambda (sr k)
       (m sr (lambda (v)
@@ -164,6 +168,10 @@
   (let ((level (length g.init)))
     (set! g.init (cons (cons n `(predefined . ,level)) g.init))
     level))
+
+(define g.current '())
+
+(define g.init '())
 
 (define sg.current (make-vector 100))
 
@@ -229,6 +237,30 @@
               (k value)))))
       (static-wrong "No such variable" n))))
 
+(define (meaning-assignment n e r)
+  (let ((m (meaning e r))
+         (kind (compute-kind r n)))
+    (if kind
+      (case (car kind)
+        ((local)
+          (let ((i (cadr kind))
+                 (j (cddr kind)))
+            (if (= i 0)
+              (lambda (sr k)
+                (m sr (lambda (v)
+                        (k (set-activation-frame-argument! sr j v)))))
+              (lambda (sr k)
+                (m sr (lambda (v)
+                        (k (deep-update! sr i j v))))))))
+        ((global)
+          (let ((i (cdr kind)))
+            (lambda (sr k)
+              (m sr (lambda (v)
+                      (k (global-update! i f)))))))
+        ((predefined)
+          (static-wrong "Immutable predefined variable" n)))
+      (static-wrong "No such variable" n))))
+
 (define (static-wrong message . culprits)
   (display `(*static-error* ,message . ,culprits)) (newline)
   (lambda (sr k)
@@ -238,6 +270,230 @@
 
 (define sr.init '())
 
+(define (meaning-dotted-abstraction n* n e+ r)
+  (let* ((arity (length n*))
+          (arity+1 (+ arity 1))
+          (r2 (r-extend* r (append n* (list n))))
+          (m+ (meaning-sequence e+ r2)))
+    (lambda (sr k)
+      (k (lambda (v* k1)
+           (if (>= (activation-frame-argument-length v*) arity+1)
+             (begin (listify! v* arity)
+               (m+ (sr-extend* sr v* k1)))
+             (wrong "Incorrect arity")))))))
+
+(define (listify! v* arity)
+  (let loop ((index (- (activation-frame-argument-length v*) 1))
+              (result '()))
+    (if (= arity index)
+      (set-activation-frame-argument! v* arity result)
+      (loop (- index 1)
+        (cons (activation-frame-argument v* (- index 1))
+          result)))))
+
+(define (meaning-abstraction nn* e+ r)
+  (let parse ((n* nn*)
+               (regular '()))
+    (cond
+      ((pair? n*) (parse (cdr n*) (cons (car n*) regular)))
+      ((null? n*) (meaning-fix-abstraction nn* e+ r))
+      (else (meaning-dotted-abstraction (reverse regular) n* e+ r)))))
+
+(define (meaning-closed-application e ee* r)
+  (let ((nn* (cadr e)))
+    (let parse ((n* nn*)
+                 (e* ee*)
+                 (regular '()))
+      (cond ((pair? n*)
+              (if (pair? e*)
+                (parse (cdr n*) (cdr e*) (cons (car n*) regular))
+                (static-wrong "Too less arguments" e ee*)))
+        ((null? n*)
+          (if (null? e*)
+            (meaning-fix-closed-application
+              nn* (cddr e) ee* r)
+            (static-wrong "Too much arguments" e ee*)))
+        (else (meaning-dotted-closed-application
+                (reverse regular) n* (cddr e) ee* r))))))
+
+(define (meaning-fix-closed-application n* body e* r)
+  (let* ((m* (meaning* e* r (length e*)))
+          (r2 (r-extend* r n*))
+          (m+ (meaning-sequence body r2)))
+    (lambda (sr k)
+      (m* sr (lambda (v*)
+               (m+ (sr-extend* sr v*) k))))))
+
+(define (meaning-dotted-closed-application n* n body e* r)
+  (let* ((m* (meaning-dotted* e* r (length e*) (length n*)))
+          (r2 (r-extend* r (append n* (list n))))
+          (m+ (meaning-sequence body r2)))
+    (lambda (sr k)
+      (m* sr (lambda (v*)
+               (m+ (sr-extend* sr v*) k))))))
+
+(define (meaning-dotted* e* r size arity)
+  (if (pair? e*)
+    (meaning-some-dotted-arguments (car e*) (cdr e*) r size arity)
+    (meaning-no-dotted-argument r size arity)))
+
+(define (meaning-some-dotted-arguments e e* r size arity)
+  (let ((m (meaning e r))
+         (m* (meaning-dotted* e* r size arity))
+         (rank (- size (+ (length e*) 1))))
+    (if (< rank arity)
+      (lambda (sr k)
+        (m sr (lambda (v)
+                (m* sr (lambda (v*)
+                         (set-activation-frame-argument! v* rank v)
+                         (k v*))))))
+      (lambda (sr k)
+        (m sr (lambda (v)
+                (m* sr (lambda (v*)
+                         (set-activation-frame-argument!
+                           v* arity
+                           (cons v (activation-frame-argument
+                                     v* arity)))
+                         (k v*)))))))))
+
+(define (meaning-no-dotted-argument r size arity)
+  (let ((arity+1 (+ arity 1)))
+    (lambda (sr k)
+      (let ((v* (allocate-activation-frame arity+1)))
+        (set-activation-frame-argument! v* arity '())
+        (k v*)))))
+
+(define-syntax definitial
+  (syntax-rules ()
+    ((definitial name value)
+      (g.init-initialize! 'name value))))
+
+(define-syntax defprimitive
+  (syntax-rules ()
+    ((defprimitive name value 0) (defprimitive0 name value))
+    ((defprimitive name value 1) (defprimitive1 name value))
+    ((defprimitive name value 2) (defprimitive2 name value))
+    ((defprimitive name value 3) (defprimitive3 name value))))
+
+(define-syntax defprimitive0
+  (syntax-rules ()
+    ((defprimitive0 name value)
+      (definitial name
+        (letrec ((arity+1 (+ 0 1))
+                  (behavior
+                    (lambda (v* k)
+                      (if (= (activation-frame-argument-length v*)
+                            arity+1)
+                        (k (value))
+                        `(,wrong "Incorrect arity" name)))))
+          `(,description-extend!
+             name (function ,value a b c))
+          behavior)))))
+
+(define-syntax defprimitive1
+  (syntax-rules ()
+    ((defprimitive1 name value)
+      (definitial name
+        (letrec ((arity+1 (+ 1 1))
+                  (behavior
+                    (lambda (v* k)
+                      (if (= (activation-frame-argument-length v*)
+                            arity+1)
+                        (k (value (activation-frame-argument v* 0)))
+                        `(,wrong "Incorrect arity" name)))))
+          `(,description-extend!
+             name (function ,value a b c))
+          behavior)))))
+
+(define-syntax defprimitive2
+  (syntax-rules ()
+    ((defprimitive2 name value)
+      (definitial name
+        (letrec ((arity+1 (+ 2 1))
+                  (behavior
+                    (lambda (v* k)
+                      (if (= (activation-frame-argument-length v*)
+                            arity+1)
+                        (k (value (activation-frame-argument v* 0)
+                             (activation-frame-argument v* 1)))
+                        `(,wrong "Incorrect arity" name)))))
+          `(,description-extend!
+             name (function ,value a b c))
+          behavior)))))
+
+(define-syntax defprimitive3
+  (syntax-rules ()
+    ((defprimitive3 name value)
+      (definitial name
+        (letrec ((arity+1 (+ 3 1))
+                  (behavior
+                    (lambda (v* k)
+                      (if (= (activation-frame-argument-length v*)
+                            arity+1)
+                        (k (value (activation-frame-argument v* 0)
+                             (activation-frame-argument v* 1)
+                             (activation-frame-argument v* 2)))
+                        `(,wrong "Incorrect arity" name)))))
+          `(,description-extend!
+             name (function ,value a b c))
+          behavior)))))
+
+(define desc.init '())
+
+(define (description-extend! name description)
+  (set! desc.init (cons (cons name description) desc.init))
+  name)
+
+(define (get-description name)
+  (let ((p (assq name desc.init)))
+    (and (pair? p) (cdr p))))
+
+(define (meaning-application e e* r)
+  (cond
+    ((and (symbol? e)
+       (let ((kind (compute-kind r e)))
+         (and (pair? kind)
+           (eq? 'predefined (car kind))
+           (let ((desc (get-description e)))
+             (and desc
+               (eq? 'function (car desc))
+               (if (= (length (cddr desc)) (length e*))
+                 (meaning-primitive-application e e* r)
+                 (static-wrong "Incorrect arity for" e))))))))
+    ((and (pair? e)
+       (eq? 'lambda (car e)))
+      (meaning-closed-application e e* r))
+    (else (meaning-regular-application e e* r))))
+
+(define (meaning-primitive-application e e* r)
+  (let* ((desc (get-description e))     ; desc = (function address . variables-list)
+          (address (cadr desc))
+          (size (length e*)))
+    (case size
+      ((0) (lambda (sr k) (k (address))))
+      ((1)
+        (let ((m1 (meaning (car e*) r)))
+          (lambda (sr k)
+            (m1 sr (lambda (v)
+                     (k (address v)))))))
+      ((2)
+        (let ((m1 (meaning (car e*) r))
+               (m2 (meaning (cadr e*) r)))
+          (lambda (sr k)
+            (m1 sr (lambda (v1)
+                     (m2 sr (lambda (v2)
+                              (k (address v1 v2)))))))))
+      ((3)
+        (let ((m1 (meaning (car e*) r))
+               (m2 (meaning (cadr e*) r))
+               (m3 (meaning (caddr e*) r)))
+          (lambda (sr k)
+            (m1 sr (lambda (v1)
+                     (m2 sr (lambda (v2)
+                              (m3 sr (lambda (v3)
+                                       (k (address v1 v2 v3)))))))))))
+      (else (meaning-regular-application e e* r)))))
+
 (define (chapter61-interpreter)
   (define (compile e) (meaning e r.init))
   (define (run c) (c sr.init display))
@@ -245,6 +501,7 @@
     (run (compile (read)))
     (toplevel))
   (toplevel))
+
 
 (definitial t #t)
 
@@ -273,3 +530,8 @@
             frame)
           k)
         (wrong "Incorrect arity" 'call/cc)))))
+
+(define-syntax defvariable
+  (syntax-rules ()
+    ((defvariable name) (g.current-initialize! 'name))))
+
